4238b54c0edf628383a78455948c1e09d2668f1b
matheustm23 <matheus.tm@aluno.ifsc.edu.br>
2026-06-12 15:56:34 -0300

    Adicionado pasta da equipe 03

---
+++ equipes/equipe_03/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 6/12/2026 3:12:05 PM
+ *  Author: Matheus
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
